777b65654f5014a29cf94286f1cb098e60fdcb09
RafikSameh <rafiksameh123@gmail.com>
2023-06-14 14:11:07 -0700

    data_structures_tasks

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new = set(my_list)
-    for i in range(len(new)):
-        sum += new[i]
+    sum = 0
+    for i in new:
+        sum += i
     return (sum)
