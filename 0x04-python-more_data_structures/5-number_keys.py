09ce35bdbdf9482fb36e9260b91b5e432e9daf8f
RafikSameh <rafiksameh123@gmail.com>
2023-06-14 14:27:53 -0700

    data_structures_tasks

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for i in enumerate(a_dictionary)
-    return (i+1)
+    num = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num += 1
+
+    return (num)
