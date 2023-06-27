8c602d1da9c2ca67db3ecf15812f877edbe07eb7
RafikSameh <rafiksameh123@gmail.com>
2023-06-27 16:52:06 -0700

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square class """
+
+
+class Square:
+    """represent a square class"""
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if not isinstance(size, int):
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
