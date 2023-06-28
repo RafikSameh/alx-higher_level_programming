700de72f6fb6b564273c0b34e778fca4fff9a56f
RafikSameh <rafiksameh123@gmail.com>
2023-06-27 17:14:16 -0700

    5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -35,5 +35,5 @@ class Square:
             for j in range(self.__size):
                 print("#", end="")
             print("")
-        if size == 0:
+        if self.__size == 0:
             print("")
