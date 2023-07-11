ccfccf55ace9d29272945d7d0a4601899ffdf2fd
RafikSameh <rafiksameh123@gmail.com>
2023-07-11 15:10:37 -0700

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""check the instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function check the instance"""
+    if isintance(obj, a_class):
+        return True
+    return False
