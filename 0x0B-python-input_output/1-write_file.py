e2b2254ec6c51e03382f0aa2076efce879f1250c
RafikSameh <rafiksameh123@gmail.com>
2023-07-11 16:45:13 -0700

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """function to write in a file"""
     with open(filename, "w", encoding="utf-8") as f:
-        return f.wrtie(text)
+        return f.write(text)
