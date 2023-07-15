15788ce47e14d96ee4cf9e64c4fbb25c745fd2a0
RafikSameh <rafiksameh123@gmail.com>
2023-07-14 20:27:08 -0700

    2-3-4-5-6

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""define a rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """derive a rectangle class"""
+    def __int__(self, width, height, x, y):
+        """class initialization"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__Y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """calculate the area of rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """displays the rectangle in #"""
+        if self.height == 0 or self.width == 0:
+            print("")
+            return
+        for i in range(self.height):
+            [print("#") for j in range(self.width)]
+            print("")
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
